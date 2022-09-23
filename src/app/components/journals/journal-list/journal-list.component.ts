8033d504cdb9cb07b2e30b2bd393b3d07af34e7a
jasondunken <jasondunken@yahoo.com>
2022-09-23 15:46:41 -0400

    extend journal list component

--- src/app/components/journals/journal-list/journal-list.component.ts
+++ src/app/components/journals/journal-list/journal-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { JournalService } from "src/app/services/journal.service";
+import { CreateJournalDto, JournalDto } from "src/app/models/journal.model";
 
 @Component({
     selector: "app-journal-list",
@@ -10,12 +13,38 @@ import { JournalService } from "src/app/services/journal.service";
 export class JournalListComponent implements OnInit {
     journals;
 
-    constructor(private journalService: JournalService) {}
+    creatingJournal: boolean = false;
+    createJournalForm: FormGroup;
+
+    constructor(private journalService: JournalService, private fb: FormBuilder, private router: Router) {}
 
     ngOnInit(): void {
         this.journalService.findAll().subscribe((journals) => {
             this.journals = journals;
             console.log("journals: ", journals);
         });
+        this.createJournalForm = this.fb.group({
+            name: ["", Validators.required],
+            description: ["", Validators.required],
+        });
+    }
+
+    addNewJournal(): void {
+        this.creatingJournal = true;
+    }
+
+    createJournal(): void {
+        if (this.createJournalForm.valid) {
+            const journal: CreateJournalDto = this.createJournalForm.value;
+            this.journalService.create(journal).subscribe((journal: JournalDto) => {
+                console.log("new journal created: ", journal);
+                this.creatingJournal = false;
+                this.router.navigateByUrl(`journals/${journal._id}`);
+            });
+        }
+    }
+
+    cancelCreateJournal(): void {
+        this.creatingJournal = false;
     }
 }
